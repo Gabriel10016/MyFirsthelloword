435790e1bacf427b4f44e5d760af2653670ece3e
Gabriel Carpio <gcarpio12@outlook.es>
2024-10-31 20:40:21 -0500

    NOtas

---
+++ Punto2_40/Program.cs
@@ -0,0 +1,60 @@
+﻿internal class NOTAS
+
+{
+    static void Main()
+    {
+        bool SG = true;
+
+        while (SG)
+        {
+            List<double> prom = new List<double>();
+            int EG = 0;
+            int EP = 0;
+
+            Console.WriteLine("Digite los estudiantes a promediar:");
+            int CE = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= CE; i++)
+            {
+                Console.WriteLine($"Digite la nota del estudiante numero {i}:");
+                double N = double.Parse(Console.ReadLine());
+
+                prom.Add(N);
+            }
+
+            foreach (double pro in prom)
+            {
+                string cat;
+                if (pro >= 4.5)
+                {
+                    cat = "Excelente";
+                    EG++;
+                }
+                else if (pro >= 4.0)
+                {
+                    cat = "Sobresaliente";
+                    EG++;
+                }
+                else if (pro >= 3.5)
+                {
+                    cat = "Bueno";
+                    EG++;
+                }
+                else
+                {
+                    cat = "Insuficiente";
+                    EP++;
+                }
+
+                Console.WriteLine($"El promedio del estudiante es: {pro}");
+            }
+
+            Console.WriteLine($"\nLos estudiantes que ganaron son: {EG}");
+            Console.WriteLine($"Los estudiantes que perdieron son: {EP}");
+
+            Console.WriteLine("\nSi deseas calcular otros estuciantes preciona (S) de otro modo (N)");
+            string R = Console.ReadLine().ToLower();
+            SG = (R== "s");
+        }
+    }
+}
---
+++ taller_parcial2/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+class Estudiante
+{
+    public int Cred{ get; set; }
+    public int Est{ get; set; }
+    public const decimal ValCNormal = 100000; 
+
+    public decimal CMatricula()
+    {
+        decimal mBase;
+
+        if (Cred <= 20)
+        {
+            mBase = Cred * ValCNormal;
+        }
+        else
+        {
+            mBase = (20 * ValCNormal) + ((Cred- 20) * ValCNormal * 2);
+        }
+        decimal desc= 0;
+        switch (Est)
+        {
+            case 1:
+                desc = mBase * 0.80m;
+                break;
+            case 2:
+                desc = mBase * 0.50m; 
+                break;
+            case 3:
+                desc = mBase * 0.30m;
+                break;
+        }
+        return mBase - desc;
+    }
+    public decimal CalSub()
+    {
+        switch (Est)
+        {
+            case 1:
+                return 200000; 
+            case 2:
+                return 100000; 
+            default:
+                return 0; 
+        }
+    }
+}
+class Program
+{
+    static void Main()
+    {
+        do
+        {
+            Console.Clear();
+            Estudiante estudiante = new Estudiante();
+            Console.WriteLine("Digite la cantidad de créditos: ");
+            estudiante.Cred = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite el numero del estrato (1, 2 o 3): ");
+            estudiante.Est = int.Parse(Console.ReadLine());
+            decimal matricula = estudiante.CMatricula();
+            decimal subsidio = estudiante.CalSub();
+            Console.WriteLine($"El valor a pagar es de: {matricula:C}, menos el valor de su subsidio cual es de: {subsidio:C}");
+            Console.WriteLine("¿Desea calcular la matrícula de otro estudiante? (s/n): ");
+        } while (Console.ReadLine().ToLower() == "s");
+    }
+}
